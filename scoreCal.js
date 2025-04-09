3a9a01a26067477cb6ac9765d6435d6419879a83
kelibien <116824137+kelibien@users.noreply.github.com>
2025-04-09 11:04:46 +0700

    New file scoreCal.js

---
+++ scoreCal.js
@@ -0,0 +1,19 @@
+function calculateGrade(score) {
+    if (score >= 80 && score <= 100)
+      return "Grade A";
+    else if (score >= 70 && score <= 79)
+      return "Grade B";
+    else if (score >= 60 && score <= 69)
+      return "Grade C";
+    else if (score >= 50 && score <= 59)
+      return "Grade D";
+    else if (score < 50)
+      return "Grade F";
+    else
+      return "Invalid score"; // บั๊ก 1: เงื่อนไขนี้ไม่เคยถูกเรียกใช้
+  }
+  
+  console.log(calculateGrade(85)); // ควรได้ A
+  console.log(calculateGrade(73)); // ควรได้ B
+  console.log(calculateGrade("90")); // บั๊ก 2: ส่ง string เข้ามา แต่ยังไม่แปลงเป็นตัวเลข
+  
